b9d903e9638e6a30ba2470a7b68f54705f555a29
wesbin <leegosoo11@naver.com>
2023-11-04 14:17:04 +0900

    1.2 Dynamic type

--- dart/bin/main.dart
+++ dart/bin/main.dart
@@ -1,6 +1,13 @@
 void main() {
-  // var name = 'empty castle'; 함수, 지역변수
-  String name = 'empty castle'; // class property
-  // name = 1; type false
-  name = 'test';
+  dynamic name;
+  // var name;
+  name = 'castle';
+  if(name is String) {
+    print(name.length);
+  }
+  name = 12;
+  if(name is int) {
+    print(name.isEven);
+  }
+  name = true;
 }
