95bcb1f7b0dd77bd1a1a4e82ceb0e04b29331e89
wesbin <leegosoo11@naver.com>
2023-11-15 21:09:12 +0900

    5.0 end

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,82 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        textTheme: TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+          displayMedium: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? MyLargeTitle() : Text("nothing"),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: Icon(Icons.remove_red_eye),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    super.initState();
-    print("init State");
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print("dispose!");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "My Large Title",
-      style: TextStyle(
-          fontSize: 30, color: Theme.of(context).textTheme.titleLarge?.color),
+      home: const HomeScreen(),
     );
   }
 }
